7a966ca3e34bd32bd289adeb2f60845560890568
muhsink7 <muhasink444@gmail.com>
2023-05-15 19:26:14 -0700

    fourth commit

--- lib/constants/auth_constants.dart
+++ lib/constants/auth_constants.dart
@@ -6,8 +6,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp();
+
 FirebaseAuth auth = FirebaseAuth.instance;
+
 FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
 GoogleSignIn googleSign = GoogleSignIn();
 
 
